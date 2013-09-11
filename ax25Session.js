e08dc43d6eb6000ebd035af726d7900a1c4ed2a6
echicken <echicken@bbs.electronicchicken.com>
2013-09-10 22:16:46 -0400

    Added ax25Session.js to module.

---
+++ ax25Session.js
@@ -0,0 +1,7 @@
+var ax25Defs = require("./ax25defs.js").ax25Defs;
+
+var ax25Session = function() {
+
+}
+
+exports.ax25Session = ax25Session;
--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.kissDefs	= require("./kissdefs.js").kissDefs;
 exports.ax25Defs	= require("./ax25defs.js").ax25Defs;
 exports.kissTNC		= require("./kissTNC.js").kissTNC;
 exports.ax25Packet	= require("./ax25Packet.js").ax25Packet;
+exports.ax25Session	= require("./ax25Session.js").ax25Session;
