e8d7e20e04be022dd3e419c68fdb950ec7bf45f4
echicken <echicken@chickenbook.(none)>
2014-06-15 23:46:00 -0400

    Tabs were motherfucked.

--- examples/echo.js
+++ examples/echo.js
@@ -1,65 +1,65 @@
 /*  AX.25 echo server example
-    Listens for connections on a single TNC destined for a single callsign +
-    SSID pair, echoes whatever the client sends back to it. */
+Listens for connections on a single TNC destined for a single callsign +
+SSID pair, echoes whatever the client sends back to it. */
 
 var ax25 = require("../index.js"),
-    util = require("util");
+util = require("util");
 
 var myCallsign = "VE3XEC";
 var mySSID = 0;
 var tnc = new ax25.kissTNC(
-    {   serialPort : "/dev/ttyUSB1",
-        baudRate : 9600
-    }
+	{	serialPort : "/dev/ttyUSB1",
+		baudRate : 9600
+	}
 );
 
 var sessions = {};
 
 tnc.on(
-    "frame",
-    function(frame) {
+	"frame",
+	function(frame) {
 
-        var packet = new ax25.Packet();
+		var packet = new ax25.Packet();
 		packet.disassemble(frame);
-        if( packet.destinationCallsign != myCallsign
-            ||
-            packet.destinationSSID != mySSID
-        ) {
-            return;
-        }
+		if( packet.destinationCallsign != myCallsign
+			||
+			packet.destinationSSID != mySSID
+		) {
+			return;
+		}
 
-        console.log(packet.log());
+		console.log(packet.log());
 
-        var clientID = util.format(
-            "%s-%s-%s-%s",
-            packet.sourceCallsign,
-            packet.sourceSSID,
-            packet.destinationCallsign,
-            packet.destinationSSID
-        );       
+		var clientID = util.format(
+			"%s-%s-%s-%s",
+			packet.sourceCallsign,
+			packet.sourceSSID,
+			packet.destinationCallsign,
+			packet.destinationSSID
+		);
 
 		if(typeof sessions[clientID] == "undefined") {
 
 			sessions[clientID] = new ax25.Session();
 
 			sessions[clientID].on(
-                "packet",
-                function(frame) {
-                    console.log(frame.log());
-                    tnc.send(frame.assemble());
-                }
-            );
+				"packet",
+				function(frame) {
+					console.log(frame.log());
+					tnc.send(frame.assemble());
+				}
+			);
 
 			sessions[clientID].on(
-                "data",
-                function(data) {
-                    sessions[clientID].sendString(
-                        util.format(
-                            "You sent: %s\r\n",
-                            ax25.Utils.byteArrayToString(data)
-                        )
-                    );
-                }
+				"data",
+				function(data) {
+					sessions[clientID].sendString(
+						util.format(
+							"You sent: %s\r\n",
+							ax25.Utils.byteArrayToString(data)
+						)
+					);
+				}
 			);
 
 			sessions[clientID].on(
@@ -68,8 +68,8 @@ tnc.on(
 					console.log(
 						util.format(
 							"Client %s-%s %s.",
-                            packet.sourceCallsign,
-                            packet.sourceSSID,
+							packet.sourceCallsign,
+							packet.sourceSSID,
 							(state) ? "connected" : "disconnected"
 						)
 					);
@@ -78,25 +78,25 @@ tnc.on(
 				}
 			);
 
-            sessions[clientID].on(
-                "error",
-                function(err) {
-                    console.log(err);
-                }
-            );
+			sessions[clientID].on(
+				"error",
+				function(err) {
+					console.log(err);
+				}
+			);
 
 		}
 
 		if(typeof sessions[clientID] != "undefined")
-    		sessions[clientID].receive(packet);
+			sessions[clientID].receive(packet);
 
-    }
+	}
 
 );
 
 tnc.on(
-    "error",
-    function(err) {
-        console.log("HURRRRR! I DONE BORKED! " + err);
-    }
+	"error",
+	function(err) {
+		console.log("HURRRRR! I DONE BORKED! " + err);
+	}
 );
