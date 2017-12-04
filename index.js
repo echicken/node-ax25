2a76fae720f18c3444605cc581a9dc12f9d6de43
echicken <echicken@bbs.electronicchicken.com>
2017-12-04 11:21:22 -0500

    Moving stuff around

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-exports.kissDefs	= require("./kissDefs.js");
-exports.Defs		= require("./Defs.js");
-exports.Utils		= require("./Utils.js");
-exports.kissTNC		= require("./kissTNC.js");
-exports.Packet		= require("./Packet.js");
-exports.Session		= require("./Session.js");
+'use strict';
+const path = require('path');
+
+exports.Masks = require(path.join(__dirname, 'masks.js'));
+exports.Packet = require(path.join(__dirname, 'packet.js'));
